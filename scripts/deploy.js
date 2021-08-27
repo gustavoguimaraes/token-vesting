112dcc204d62252dafffc26b620833a0de6a17c4
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2021-08-27 20:52:42 +0200

    modify deploy script to include TokenVesting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { ethers } = hre;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const startBlock = (await ethers.provider.getBlock()).number;
+  const endBlock = startBlock + 1000;
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MockERC20 = await ethers.getContractFactory("MockERC20");
+  const testToken = await MockERC20.deploy("Test", "VEST");
+  testToken.deployTransaction.wait();
 
-  await greeter.deployed();
+  const TokenVesting = await hre.ethers.getContractFactory("TokenVesting");
+  const tokenVesting = await TokenVesting.deploy(
+    startBlock,
+    endBlock,
+    testToken.address
+  );
+  tokenVesting.deployTransaction.wait();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("TokenVesting deployed to:", tokenVesting.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
